0bfbe08086503f35e20d42cf326c4fbdd2f5d757
wangwenqiangGitHub <1103169969@qq.com>
2022-05-01 11:20:50 +0800

    提取vue组件

---
+++ src/components/form/hd-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="hd-button mt-5">登录</button>
+</template>
+
+<style lang="scss">
+.hd-button {
+  @apply bg-indigo-500 text-white w-full py-2 rounded-md mt-3 hover:bg-violet-500 duration-300;
+}
+</style>
---
+++ src/components/form/hd-link.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a href class="text-sm text-gray-700 hover:text-violet-700">网站首页</a>
+</template>
+
+<style lang="scss"></style>
