0d7d38338859db644659bc1d0383467c72914e6d
wangwenqiangGitHub <1103169969@qq.com>
2022-05-03 09:09:41 +0800

    修改登录图片自适应

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -22,22 +22,26 @@ const schema = {
 <template>
   <Form class="" @submit="onSubmit" :validation-schema="schema" #default="{ errors }">
     <div class="w-[720px] translate-y-32 md:translate-y-0 bg-white grid md:grid-cols-2 rounded-md shadow-md">
-      <div class="p-6">
-        <h2 class="text-center text-gray-700 text-lg">会员登录</h2>
-        <div class="mt-8">
-          <Field
-            name="account"
-            class="hd-input"
-            :validate-on-input="true"
-            label="账号"
-            placeholder="请输入邮箱或者手机号" />
-          <div v-if="errors.account" class="hd-error">请输入邮箱或者手机号</div>
-          <!-- <ErrorMessage name="account" as="div" class="hd-error" /> -->
-          <Field name="password" class="hd-input mt-3" :validate-on-input="true" label="密码" type="password" />
-          <ErrorMessage name="password" as="div" class="hd-error" />
+      <div class="p-6 flex flex-col justify-between">
+        <div>
+          <h2 class="text-center text-gray-700 text-lg">会员登录</h2>
+          <div class="mt-8">
+            <Field
+              name="account"
+              class="hd-input"
+              :validate-on-input="true"
+              label="账号"
+              placeholder="请输入邮箱或者手机号" />
+            <div v-if="errors.account" class="hd-error">请输入邮箱或者手机号</div>
+            <!-- <ErrorMessage name="account" as="div" class="hd-error" /> -->
+            <Field name="password" class="hd-input mt-3" :validate-on-input="true" label="密码" type="password" />
+            <ErrorMessage name="password" as="div" class="hd-error" />
+          </div>
+          <hdButton />
+          <div class="flex justify-center mt-3">
+            <i class="fa-brands fa-weixin bg-green-600 text-white rounded-full p-1 cursor-pointer"></i>
+          </div>
         </div>
-        <!-- <button class="hd-button mt-5">登录</button> -->
-        <hdButton />
         <div class="flex gap-2 justify-center mt-5">
           <!-- <a href class="text-sm">网站首页</a> -->
           <hdLink />
@@ -45,8 +49,8 @@ const schema = {
           <a href class="text-sm">找回密码</a>
         </div>
       </div>
-      <div class="hidden md:block">
-        <img src="/images/login.jpg" class="h-96 w-full object-cover" />
+      <div class="hidden md:block relative">
+        <img src="/images/login.jpg" class="absolute h-full w-full object-cover" />
       </div>
     </div>
   </Form>
